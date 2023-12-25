a4e62339cb697333be05617908eddf3b313453f1
@ethkimchhay03 <ethkimchhay03@gmail.com>
2023-12-25 14:27:25 +0700

    update index.js

--- my-package/dist/index.js
+++ my-package/dist/index.js
@@ -1,8 +1,42 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convert = void 0;
-function convert(amount, fromCurrency, toCurrency) {
-  return amount; // Placeholder, replace with actual conversion logic
-}
+"use strict"
+// Object.defineProperty(exports, "__esModule", {value: true})
+// exports.convert = void 0
+// function convert(amount, fromCurrency, toCurrency) {
+//     return amount // Placeholder, replace with actual conversion logic
+// }
 
-exports.convert = convert;
+// exports.convert = convert
+class CurrencyConverter {
+    constructor() {
+        this.baseCurrency = "USD"
+    }
+
+    async getExchangeRates() {
+        const response = await fetch("https://open.er-api.com/v6/latest")
+        const data = await response.json()
+
+        if (data.error) {
+            throw new Error(
+                `Failed to fetch exchange rates: ${data.error.message}`,
+            )
+        }
+
+        this.exchangeRates = data.rates
+    }
+
+    async convert(amount, fromCurrency, toCurrency) {
+        if (!this.exchangeRates) {
+            await this.getExchangeRates()
+        }
+
+        const fromRate = this.exchangeRates[fromCurrency]
+        const toRate = this.exchangeRates[toCurrency]
+
+        if (!fromRate || !toRate) {
+            throw new Error("Invalid currency code")
+        }
+
+        const convertedAmount = (amount / fromRate) * toRate
+        return convertedAmount
+    }
+}
